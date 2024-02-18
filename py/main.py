dc633471d02659b7add60d278ff4de31e2767bb2
tabf129 <84515498+tabf129@users.noreply.github.com>
2024-02-18 06:34:49 +0000

    python code

--- py/main.py
+++ py/main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, request
+from transcribe import transcribe_audio
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/transcribe", methods=["POST"])
 def index():
-    return "Hello World"
+    if request.method == "POST":
+        data = request.json
+        transcribe_audio(data["audio"])
+
+    return "Request has been received"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
