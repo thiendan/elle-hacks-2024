0170c8e9363c0cb73a167e7528562db84d55920c
thiendan <98007308+thiendan@users.noreply.github.com>
2024-02-18 06:57:41 +0000

    generating GPT questions

---
+++ py/generateQuestions.py
@@ -0,0 +1,16 @@
+from openai import OpenAI
+client = OpenAI()
+
+def generate_questions(JOB_ROLE, JOB_DESCRIPTION):
+
+    # making request to GPT
+    response = client.chat.completions.create(
+    model="gpt-4-turbo-preview",
+    messages=[
+        {"role": "system", "content": "${JOB_ROLE}, please generate 10 questions to ask the interviewee. ${JOB_DESCRIPTION}"}
+    ]
+    )
+
+    # returning the GPT response
+    return completion.choices[0].message.content
+    
