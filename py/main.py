770412030a2b2dc4938525fdb66b050b61fb1db4
tabf129 <84515498+tabf129@users.noreply.github.com>
2024-02-18 10:51:12 +0000

    add generateRoute & update prompts

--- py/main.py
+++ py/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from transcribe import transcribe_audio
+from generateQuestions import generate_questions
 
 app = Flask(__name__)
 
@@ -7,9 +8,20 @@ app = Flask(__name__)
 def index():
     if request.method == "POST":
         data = request.json
-        transcribe_audio(data["audio"])
+        if "audio" in data:
+            return transcribe_audio(data["audio"])
 
-    return "Request has been received"
+    return "Error - Something went wrong."
+
+
+@app.route("/generateQuestions", methods=["POST"])
+def getQuestions():
+    if request.method == "POST":
+        data = request.json
+        if "job_description" in data and "role" in "data":
+            role = data["role"]
+            job_description = data["job_description"]
+            return generate_questions(role, job_description)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
